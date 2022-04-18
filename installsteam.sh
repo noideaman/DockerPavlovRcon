1ba00468569d8d4fe3e05c6c5f00a828e3413cbd
noideaman <noid@weebvr.com>
2022-04-18 12:43:42 -0600

    added comments to describe scripts and what is going on

--- installsteam.sh
+++ installsteam.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#allow steam access to sudo with out password prompts
 sed -i s/\%sudo.*// /etc/sudoers
 echo "%sudo        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+#set debconf settings to skip prompts for steamcmd so the build proccess does not hang
 echo steam steam/question select "I AGREE" | debconf-set-selections
 echo steam steam/license note '' | debconf-set-selections
+#install steamcmd and dependices
 apt update -y
 apt install lib32gcc-s1 steamcmd -y
--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #/bin/bash
+#download the latest release of PavlovRconWebserver
 cd /
+#if exists grab latest version, else download fresh version
 if [ -d "/PavlovRconWebserver/" ]
 then
         cd /PavlovRconWebserver/
@@ -8,15 +10,23 @@ then
 else
         git clone https://github.com/devinSpitz/PavlovRconWebserver.git
 fi
+#cd into directory and build latest version
 cd /PavlovRconWebserver/PavlovRconWebserver/
 dotnet publish -c release -o /build --runtime linux-x64 --self-contained true --framework net5.0
 cd /build/
+#copy default database if not existant
 [ ! -f Database/Database.db ] && cp /PavlovRconWebserver/PavlovRconWebserver/DefaultDB/Database.db /build/Database/Database.db
+#plan to set this to a docker enviroment var
 export ASPNETCORE_ENVIRONMENT=Development
+#copy the develponent.json to production.json also create a symlink as for some reason it looks for appsettings..json
+#instead of appsettings.Production.json
 [ ! -f appsettings.Production.json ] && cp /PavlovRconWebserver/PavlovRconWebserver/appsettings.Development.json /build/appsettings.Production.json
 ln -s /build/appsettings.Production.json /build/appsettings..json
 cd /
-
+#ssh host and client keys are deleted by default in container to prep for shiping
+#generate new host keys if non-existant
+#set a random password for the steam user
+#install the ssh client key to the steam user
 if [ ! -f /etc/ssh/ssh_host_dsa_key ]
 then
     dpkg-reconfigure openssh-server
@@ -28,9 +38,12 @@ then
     sshpass -f sshpass.txt ssh-copy-id -p 2222 steam@localhost
     rm -f sshpass.txt
 fi
+#start services, pavlov should be removed once pavlovrcon supports ubuntu init.d service files
 service ssh restart
 service pavlov start
 service pavlovrcon start
+#output the steam generated password and private key so rcon can be configured to use it
+#add plans to pre-populate the database with this info
 echo ""
 echo ""
 echo ""
@@ -40,4 +53,5 @@ echo ""
 echo ""
 echo ""
 echo ssh private key $(cat /root/.ssh/id_rsa)
+#start bash so the container does not exit and restart
 /bin/bash
